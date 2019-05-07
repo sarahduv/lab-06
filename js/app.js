4a94059bb4be877dfbac78330d90d8812773ed2b
sarahduv <sarah.duvdevani@gmail.com>
2019-05-07 16:25:59 -0700

    this is the finished table before css

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-// This is the array of all hours
-
+// This is the array of all data put through the constructor
 var allData = [];
 
-var locationNames = [];
+// This is linking the tabular data and headers to sales.html
+var locationTable = document.getElementById('location-data');
 
-var locationTable = document.getElementById('locationData');
-
+// This is an array holding all hours the stores are open
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+// This calculates a random number and is called inside the constructor
 function getRandomInt (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Constructor
 function allLocations(location, minCust, maxCust, averageSales) {
   this.location = location;
   this.minCust = minCust;
@@ -27,20 +28,42 @@ function allLocations(location, minCust, maxCust, averageSales) {
     }
   }
   this.render = function () {
+    // this is appending the hourly sales in each row
     // make a tr
     var trEl = document.createElement('tr');
     // create, content, append for "Name"
-    for (var i = 0; i < hourlySales.length; i++)
     var tdEl = document.createElement('td');
-    tdEl.textContent = hourlySales[i];
+    tdEl.textContent = this.location;
     trEl.appendChild(tdEl);
+    //
+    for (var i = 0; i < this.hourlySales.length; i++) {
+     tdEl = document.createElement('td');
+     tdEl.textContent = this.hourlySales[i];
+     trEl.appendChild(tdEl);
+    };
+
+    //this is appending the total data for each location
+    //total data --> create / content / append
+    var totalLocationHourlySales = 0;
+
+    for (i = 0; i < this.hourlySales.length; i++) {
+      totalLocationHourlySales += this.hourlySales[i];
+    }
+
+    var totalEl = document.createElement('td');
+    console.log('totalEl is', totalEl);
+    totalEl.textContent = totalLocationHourlySales;
+    trEl.appendChild(totalEl);
     // append the tr to the table
-    locationData.appendChild(trEl);
+    locationTable.appendChild(trEl);
+
   };
   allData.push(this);
 };
 
+
 console.log(allData);
+
 
 function makeHeaderRow() {
   // create the row
@@ -55,12 +78,16 @@ function makeHeaderRow() {
   thEl.textContent = hours[i];
   trEl.appendChild(thEl);
   };
+  //create, content, append
+  thEl = document.createElement('th');
+  thEl.textContent = 'Total';
+  trEl.appendChild(thEl);
   // append the row to the table
-  locationData.appendChild(trEl);
+  locationTable.appendChild(trEl);
 }
 
 
-
+// New instances for the constructor
 var pike = new allLocations('First and Pike', 23, 65, 6.3);
 var seaTac = new allLocations('SeaTac Airport', 3, 24, 3.2);
 var seattleCenter = new allLocations('Seattle Center', 11, 38, 3.7);
@@ -68,6 +95,7 @@ var capitolHill = new allLocations('Capitol Hill', 20, 38, 2.3);
 var alki = new allLocations('Alki', 2, 16, 4.6);
 
 
+// Calling the hourlySalesCalc for each location instance
 pike.hourlySalesCalc();
 console.log('Pikes hourly sales: ' + pike.hourlySales);
 seaTac.hourlySalesCalc();
@@ -79,4 +107,10 @@ console.log('Capitol Hills hourly sales: ' + capitolHill.hourlySales);
 alki.hourlySalesCalc();
 console.log('Alkis hourly sales: ' + alki.hourlySales);
 
+
 makeHeaderRow();
+pike.render();
+seaTac.render();
+seattleCenter.render();
+capitolHill.render();
+alki.render();
